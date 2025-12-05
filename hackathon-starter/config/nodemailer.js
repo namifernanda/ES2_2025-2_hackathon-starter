8d6acf82278c58606b566b1f5464b104ce3e6c83
Isa Delatore <85897897+IsaDelatore@users.noreply.github.com>
2025-12-05 06:19:27 -0300

    Update nodemailer.js
    Correção apontada pelo SonarQube

--- hackathon-starter/config/nodemailer.js
+++ hackathon-starter/config/nodemailer.js
@@ -19,29 +19,23 @@ exports.sendMail = (settings) => {
   return transporter
     .sendMail(settings.mailOptions)
     .then(() => {
-      settings.req.flash(settings.successfulType, {
-        msg: settings.successfulMsg,
-      });
+      settings.req.flash(settings.successfulType, { msg: settings.successfulMsg });
     })
     .catch((err) => {
       if (err.message === 'self signed certificate in certificate chain') {
-        console.log(
-          'WARNING: Self signed certificate in certificate chain. Retrying with the self signed certificate. Use a valid certificate if in production.',
-        );
+        console.log('WARNING: Self signed certificate in certificate chain. Retrying with the self signed certificate. Use a valid certificate if in production.');
         transportConfig.tls = transportConfig.tls || {};
         transportConfig.tls.rejectUnauthorized = false;
         transporter = nodemailer.createTransport(transportConfig);
         return transporter
           .sendMail(settings.mailOptions)
           .then(() => {
-            settings.req.flash(settings.successfulType, {
-              msg: settings.successfulMsg,
-            });
+            settings.req.flash(settings.successfulType, { msg: settings.successfulMsg });
           })
-          .catch((retryErr) => {
-            console.log(settings.loggingError, retryErr);
+          .catch((error_) => {
+            console.log(settings.loggingError, error_);
             settings.req.flash(settings.errorType, { msg: settings.errorMsg });
-            return retryErr;
+            return error_;
           });
       }
       console.log(settings.loggingError, err);
